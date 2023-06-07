dbf9cb764d054c8cd16e2fbd9bfda6c31f962872
limonpro <imlimonpro@gmail.com>
2023-06-08 00:50:51 +0600

    add query function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { useQuery } from "@tanstack/react-query"
+import { get } from "../api/api";
+import { GET_PRODUCTS } from "../api/endpoint";
+
 export default function Home(){
+    
+    const { isLoading, isError, error, data: products  } = useQuery( {queryKey: ['developerData'],  queryFn:() => get(`${GET_PRODUCTS}`)});
+      console.log('data', products);
+
     return(
-        <div> i am from home</div>
+        <div>
+
+        </div>
     )
 }
